d035c7e320e7692b626bd5d30fa707ffb1c99ade
sameeram <mgjaye@gmail.com>
2013-12-19 16:11:06 +0530

    Added swagger call

--- modules/apps/store/extensions/assets/api/themes/store/js/apiconsole.js
+++ modules/apps/store/extensions/assets/api/themes/store/js/apiconsole.js
@@ -1,4 +1,25 @@
 $(function(){
-       console.log('LOADW');
-       alert('Hello World!');
+    window.swaggerUi = new SwaggerUi({
+        discoveryUrl:"http://10.100.0.146:8280/testapi2/1.0.0",
+        dom_id:"swagger-ui-container",
+        apiKeyName: "authorization",
+        supportHeaderParams: true,
+        supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'options'],
+        onComplete: function(swaggerApi, swaggerUi){
+            if(console) {
+                console.log("Loaded SwaggerUI");
+                console.log(swaggerApi);
+                console.log(swaggerUi);
+            }
+            $('ul.endpoints').show();
+        },
+        onFailure: function(data) {
+            if(console) {
+                console.log("Unable to Load SwaggerUI");
+                console.log(data);
+            }
+        },
+        docExpansion: "none"
+    });
+    window.swaggerUi.load();
 });
