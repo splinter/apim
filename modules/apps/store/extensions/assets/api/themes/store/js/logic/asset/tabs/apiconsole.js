fbbe36c76806b89ab2fc1f316f6d5a8ffb98970a
sameeram <mgjaye@gmail.com>
2013-12-20 13:29:27 +0530

    Added comments to apiconsole.js script,added icons to overview tab,error msg to apiconsole tab and badges to throttle tab

--- modules/apps/store/extensions/assets/api/themes/store/js/logic/asset/tabs/apiconsole.js
+++ modules/apps/store/extensions/assets/api/themes/store/js/logic/asset/tabs/apiconsole.js
@@ -1,12 +1,18 @@
+/*
+ Description: The script is responsible for initializing the Swagger library after obtaining
+ the discoveryURL from container data element.
+ */
 $(function () {
-    var CONTAINER='swagger-ui-container';
-    var CONTAINER_ID='#'+CONTAINER;
-    var DATA_DISCOVERYURL_KEY='apimStoreApiDiscoveryurl';
-    var SWAGGER_SUPPORTED_SUBMIT_METHODS=['get', 'post', 'put', 'delete', 'options'];
-    var SWAGGER_API_KEY_NAME='authorization';
-    var SWAGGER_SUPPORT_HEADER_PARAMS=true;
-    var SWAGGER_DOC_EXPANSION='none';
 
+    var CONTAINER = 'swagger-ui-container';
+    var CONTAINER_ID = '#' + CONTAINER;
+    var DATA_DISCOVERYURL_KEY = 'apimStoreApiDiscoveryurl';
+    var SWAGGER_SUPPORTED_SUBMIT_METHODS = ['get', 'post', 'put', 'delete', 'options'];
+    var SWAGGER_API_KEY_NAME = 'authorization';
+    var SWAGGER_SUPPORT_HEADER_PARAMS = true;
+    var SWAGGER_DOC_EXPANSION = 'none';
+
+    //Obtain the discoveryURL from the container data- element (stored as data-apim-store-api-discoveryURL)
     var discoveryUrl = $(CONTAINER_ID).data(DATA_DISCOVERYURL_KEY) || null;
 
     //Support for IE
@@ -19,29 +25,26 @@ $(function () {
         return;
     }
 
+    //Initialize the Swagger UI library
     window.swaggerUi = new SwaggerUi({
-        discoveryUrl:discoveryUrl,
-        dom_id:CONTAINER,
+        discoveryUrl: discoveryUrl,
+        dom_id: CONTAINER,
         apiKeyName: SWAGGER_API_KEY_NAME,
         supportHeaderParams: SWAGGER_SUPPORT_HEADER_PARAMS,
         supportedSubmitMethods: SWAGGER_SUPPORTED_SUBMIT_METHODS,
-        onComplete: function(swaggerApi, swaggerUi){
-            if(console) {
-                console.log("Loaded SwaggerUI");
-                console.log(swaggerApi);
-                console.log(swaggerUi);
-            }
-           // $('ul.endpoints').show();
+        onComplete: function (swaggerApi, swaggerUi) {
+            console.log("Loaded SwaggerUI");
+            console.log(swaggerApi);
+            console.log(swaggerUi);
         },
-        onFailure: function(data) {
-            if(console) {
-                console.log("Unable to Load SwaggerUI");
-                console.log(data);
-            }
+        onFailure: function (data) {
+            console.log("Unable to Load SwaggerUI");
+            console.log(data);
         },
         docExpansion: SWAGGER_DOC_EXPANSION
     });
-    window.swaggerUi.load();
 
+    //Load the Swagger UI
+    window.swaggerUi.load();
 
 });
