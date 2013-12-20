220bb39d0af8507adab716fe750c3c9ce9af0adb
sameeram <mgjaye@gmail.com>
2013-12-20 12:53:54 +0530

    Adds support for swagger to render the api console

--- modules/apps/store/extensions/assets/api/themes/store/helpers/asset.js
+++ modules/apps/store/extensions/assets/api/themes/store/helpers/asset.js
@@ -1,7 +1,33 @@
-/**
- * Created with IntelliJ IDEA.
- * User: sameeramwso2com
- * Date: 12/20/13
- * Time: 11:49 AM
- * To change this template use File | Settings | File Templates.
+var name;
+var log = new Log();
+var that = this;
+var hps = require('/themes/store/helpers/asset.js');
+
+
+/*
+ In order to inherit all variables in the default helper
  */
+
+for (name in hps) {
+    if (hps.hasOwnProperty(name)) {
+        that[name] = hps[name];
+    }
+}
+
+var fn = that.resources;
+
+var resources = function (page, meta) {
+    var o = fn(page, meta);
+
+    o.js.push('libs/jquery.slideto.min.js');
+    o.js.push('libs/jquery.wiggle.min.js');
+    o.js.push('libs/jquery.ba-bbq.min.js');
+    o.js.push('libs/underscore-min.js');
+    o.js.push('libs/backbone-min.js');
+    o.js.push('libs/swagger.js');
+    o.js.push('libs/swagger-ui.js');
+
+    o.js.push('logic/asset/tabs/apiconsole.js');
+
+    return o;
+};
--- modules/apps/store/extensions/assets/api/themes/store/js/logic/asset/tabs/apiconsole.js
+++ modules/apps/store/extensions/assets/api/themes/store/js/logic/asset/tabs/apiconsole.js
@@ -1,3 +1,47 @@
-$(function(){
+$(function () {
+    var CONTAINER='swagger-ui-container';
+    var CONTAINER_ID='#'+CONTAINER;
+    var DATA_DISCOVERYURL_KEY='apimStoreApiDiscoveryurl';
+    var SWAGGER_SUPPORTED_SUBMIT_METHODS=['get', 'post', 'put', 'delete', 'options'];
+    var SWAGGER_API_KEY_NAME='authorization';
+    var SWAGGER_SUPPORT_HEADER_PARAMS=true;
+    var SWAGGER_DOC_EXPANSION='none';
+
+    var discoveryUrl = $(CONTAINER_ID).data(DATA_DISCOVERYURL_KEY) || null;
+
+    //Support for IE
+    var console = (typeof console == undefined) ? {log: function (msg) {
+    }} : console;
+
+    //Do not load swagger if there is no discovery URL
+    if (!discoveryUrl) {
+        console.log('Unable to locate discoveryUrl');
+        return;
+    }
+
+    window.swaggerUi = new SwaggerUi({
+        discoveryUrl:discoveryUrl,
+        dom_id:CONTAINER,
+        apiKeyName: SWAGGER_API_KEY_NAME,
+        supportHeaderParams: SWAGGER_SUPPORT_HEADER_PARAMS,
+        supportedSubmitMethods: SWAGGER_SUPPORTED_SUBMIT_METHODS,
+        onComplete: function(swaggerApi, swaggerUi){
+            if(console) {
+                console.log("Loaded SwaggerUI");
+                console.log(swaggerApi);
+                console.log(swaggerUi);
+            }
+           // $('ul.endpoints').show();
+        },
+        onFailure: function(data) {
+            if(console) {
+                console.log("Unable to Load SwaggerUI");
+                console.log(data);
+            }
+        },
+        docExpansion: SWAGGER_DOC_EXPANSION
+    });
+    window.swaggerUi.load();
+
 
 });
