3e614d55e0134eadf4fa2e848bc6d5bf341b4003
sameeram <mgjaye@gmail.com>
2014-01-09 17:16:49 +0530

    Added custom assetManager

--- modules/apps/store/extensions/assets/api/asset.js
+++ modules/apps/store/extensions/assets/api/asset.js
@@ -18,3 +18,17 @@ var assetLinks=function(user){
         ]
     }
 };
+
+var assetManager=function(manager){
+    var add=manager.add;
+    var log=new Log('asset');
+
+    log.info('Custom asset manager loaded');
+
+    //Override the add actions of the API
+    manager.add=function(options){
+        add.call(manager,options);
+    };
+
+    return manager;
+};
