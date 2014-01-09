bdd22d2031cd78f4296113de60a3ba6f0ee1b8ef
sameeram <mgjaye@gmail.com>
2014-01-09 18:22:35 +0530

    Added editable-grids library

--- modules/apps/store/extensions/assets/api/themes/store/helpers/myapps.js
+++ modules/apps/store/extensions/assets/api/themes/store/helpers/myapps.js
@@ -3,6 +3,6 @@ var log=new Log();
 var resources= function (page,meta){
 
     return{
-        js:['logic/common/console.js','logic/myapp/add-app.js','logic/myapp/list-apps.js']
+        js:['logic/common/console.js','logic/myapp/add-app.js','logic/myapp/list-apps.js','libs/editablegrid-2.0.1.js']
     } ;
 };
