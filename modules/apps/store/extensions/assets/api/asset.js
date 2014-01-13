7e426a482842f533ec3512dcd9219fddb1b6ab8d
sameeram <mgjaye@gmail.com>
2014-01-13 11:07:34 +0530

    Added subs page

--- modules/apps/store/extensions/assets/api/asset.js
+++ modules/apps/store/extensions/assets/api/asset.js
@@ -1,42 +1,45 @@
-
-
-var assetLinks=function(user){
+var assetLinks = function (user) {
 
     return{
-        title:'API' ,
-        links:[
+        title: 'API',
+        links: [
             {
-                 title:'Custom',
-                 url:'custom',
-                 path:'custom.jag'
+                title: 'Custom',
+                url: 'custom',
+                path: 'custom.jag'
             } ,
             {
-                title:'My Applications',
-                url:'myapps',
-                path:'myapps.jag'
+                title: 'My Applications',
+                url: 'myapps',
+                path: 'myapps.jag'
             },
             {
-                title:'Login',
-                url:'login',
-                path:'login.jag'
+                title: 'Login',
+                url: 'login',
+                path: 'login.jag'
             },
             {
-                url:'logout',
-                path:'logout.jag'
+                url: 'logout',
+                path: 'logout.jag'
+            },
+            {
+                title: 'Subscriptions',
+                url: 'subscriptions',
+                path: 'subscriptions.jag'
             }
         ]
     }
 };
 
-var assetManager=function(manager){
-    var add=manager.add;
-    var log=new Log('asset');
+var assetManager = function (manager) {
+    var add = manager.add;
+    var log = new Log('asset');
 
     log.info('Custom asset manager loaded');
 
     //Override the add actions of the API
-    manager.add=function(options){
-        add.call(manager,options);
+    manager.add = function (options) {
+        add.call(manager, options);
     };
 
     return manager;
